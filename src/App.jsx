0913d5791a648fee4999308448d6e2265c10dc09
Asur-Daanav <542akhil@gmail.com>
2024-10-22 15:01:29 +0530

    Environment variables and appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
+import conf from '../../blog/src/conf/config'
 import './App.css'
 
 function App() {
 
   return (
-    <h1>{console.log(import.meta.env.VITE_SOME_KEY) }</h1>
+    <h1>{console.log(conf) }</h1>
   )
 }
 
